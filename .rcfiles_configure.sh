8268f63b063172e2e9be571a0db54047c168086d
Jared Jensen <j@redj.xyz>
2017-08-09 23:42:11 -0700

    Added misc things to vimrc

--- .rcfiles_configure.sh
+++ .rcfiles_configure.sh
@@ -7,9 +7,18 @@ sudo apt-get install zsh
 chsh -s `which zsh`
 
 # Install oh-my-zsh
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O omz_setup.sh
+# Fix oh-my-zsh installer so it doesn't launch zshell
+grep -v "[ ]*env zsh$" omz_setup.sh > omz_setup_fixed.sh
+sh omz_setup_fixed.sh
+rm -f omz_setup omz_setup_fixed
 
 # Download git repository
 git init
 git remote add origin git@github.com:jaredjxyz/rcfiles.git
-git pull
+git fetch origin master
+git reset origin/master --hard
+
+# Download vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
