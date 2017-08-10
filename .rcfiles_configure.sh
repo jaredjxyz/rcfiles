d2777346b6e2ec9a67d17964114fec80a86a24d4
Jared Jensen <j@redj.xyz>
2017-08-10 00:00:44 -0700

    Added tmux and vim to installs

--- .rcfiles_configure.sh
+++ .rcfiles_configure.sh
@@ -3,15 +3,15 @@
 
 # Install zsh
 sudo apt-get update
-sudo apt-get install zsh
-chsh -s `which zsh`
+sudo apt-get install zsh -y
 
 # Install oh-my-zsh
+rm .zshrc
 wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O omz_setup.sh
 # Fix oh-my-zsh installer so it doesn't launch zshell
 grep -v "[ ]*env zsh$" omz_setup.sh > omz_setup_fixed.sh
 sh omz_setup_fixed.sh
-rm -f omz_setup omz_setup_fixed
+rm -f omz_setup.sh omz_setup_fixed.sh
 
 # Download git repository
 git init
@@ -19,6 +19,14 @@ git remote add origin git@github.com:jaredjxyz/rcfiles.git
 git fetch origin master
 git reset origin/master --hard
 
+# Download tmux
+sudo apt-get install tmux -y
+
+# Download vim
+sudo apt-get install vim -y
+
 # Download vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
+
+env zsh
