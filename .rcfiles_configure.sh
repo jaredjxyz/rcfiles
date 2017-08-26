cbcf6fac0148398d79d0898a5f440725ca9292d0
Jared Jensen <j@redj.xyz>
2017-08-25 21:35:27 -0700

    Added check to see if vundle is already installed

--- .rcfiles_configure.sh
+++ .rcfiles_configure.sh
@@ -33,7 +33,9 @@ rm -rf tmux-2.5
 sudo apt-get install vim -y
 
 # Download vundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+if [ ! -d "~/.vim/bundle/Vundle.vim" ]; then
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
 vim +PluginInstall +qall
 
 env zsh
